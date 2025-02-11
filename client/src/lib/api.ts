0df374c05894b1b4dd9162a839da2e6329a5b8d4
Priyank Shankar <shankarpriyank312002@gmail.com>
2025-02-11 23:58:21 +0900

    changes

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,1 +1,23 @@
- 
+const BASE_URL = process.env.NODE_ENV === 'production' 
+? process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : ''
+: '';
+
+export const apiRequest = async (
+method: string,
+path: string,
+body?: unknown
+) => {
+const response = await fetch(`${BASE_URL}${path}`, {
+  method,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: body ? JSON.stringify(body) : undefined,
+});
+
+if (!response.ok) {
+  throw new Error('API request failed');
+}
+
+return response.json();
+};
