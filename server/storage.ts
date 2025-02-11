117af7bb6dd22eabd64c6b7c0c0c9229de3cdc7a
rohitraj7631212 <26845375-rohitraj7631212@users.noreply.replit.com>
2025-02-11 07:09:29 +0000

    Agent query: Can you try uploading the race data JSON file and check if it navigates to the dashboard page and displays the race information with graphs?
    Refactor: Remove user management from storage layer; focus on race data.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/728a5265-f774-42bd-83a1-a1621e73cf13/ab675546-7379-4973-919a-719aae74b14e.jpg

--- server/storage.ts
+++ server/storage.ts
@@ -1,43 +1,12 @@
-import { users, type User, type InsertUser } from "@shared/schema";
 import { type RaceData, type InsertRaceData, type RaceDataJson } from "@shared/schema";
 
-// modify the interface with any CRUD methods
-// you might need
-
 export interface IStorage {
-  getUser(id: number): Promise<User | undefined>;
-  getUserByUsername(username: string): Promise<User | undefined>;
-  createUser(user: InsertUser): Promise<User>;
   getRaceData(): Promise<RaceDataJson | undefined>;
   createRaceData(data: InsertRaceData): Promise<RaceData>;
 }
 
 export class MemStorage implements IStorage {
-  private users: Map<number, User>;
   private raceData?: RaceData;
-  currentId: number;
-
-  constructor() {
-    this.users = new Map();
-    this.currentId = 1;
-  }
-
-  async getUser(id: number): Promise<User | undefined> {
-    return this.users.get(id);
-  }
-
-  async getUserByUsername(username: string): Promise<User | undefined> {
-    return Array.from(this.users.values()).find(
-      (user) => user.username === username,
-    );
-  }
-
-  async createUser(insertUser: InsertUser): Promise<User> {
-    const id = this.currentId++;
-    const user: User = { ...insertUser, id };
-    this.users.set(id, user);
-    return user;
-  }
 
   async getRaceData(): Promise<RaceDataJson | undefined> {
     return this.raceData?.data as RaceDataJson | undefined;
